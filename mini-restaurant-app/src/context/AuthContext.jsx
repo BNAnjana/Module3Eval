1091003f144e8e04e824fecfa700049e6a0186ca
BNAnjana <nagaanjana003@gmail.com>
2026-01-03 20:52:20 +0530

    twelfth commit

--- mini-restaurant-app/src/context/AuthContext.jsx
+++ mini-restaurant-app/src/context/AuthContext.jsx
@@ -5,6 +5,10 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  const login = (role, email) => {
+    setUser({ role, email });
+  };
+
   const logout = () => {
     setUser(null);
   };
