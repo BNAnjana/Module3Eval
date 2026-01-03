d396e186015b28bf220a1b6970557a23b90b11d6
BNAnjana <nagaanjana003@gmail.com>
2026-01-03 19:59:27 +0530

    fifth commit

--- mini-restaurant-app/src/App.jsx
+++ mini-restaurant-app/src/App.jsx
@@ -3,6 +3,13 @@ import Login from "./Login";
 import Admin from "./admin/dashboard";
 import Customer from "./customers/dashboard";
 
+function PrivateRoute({ children, role }) {
+  const { user } = useContext(AuthContext);
+  if (!user) return <Navigate to="/" />;
+  if (role && user.role !== role) return <Navigate to="/" />;
+  return children;
+}
+
 function App() {
 
   return (
